fe77adc7a7b2d5ee4ae9fed68822bc5a6aa3a152
manip_ego <52987267+manipEgo@users.noreply.github.com>
2022-10-19 16:30:45 +0800

    chore: indent reformat

--- chess.py
+++ chess.py
@@ -12,24 +12,26 @@ directions = ([1,0], [1,1], [0,1], [-1, 1], [-1, 0], [-1, -1], [0, -1], [1, -1])
 
 class AI(object):
         def __init__(self, chessboard_size, color, time_out):
-                self.chessboard_size = chessboard_size
-                self.color = color
-                self.time_out = time_out
-                self.candidate_list = []
+            self.chessboard_size = chessboard_size
+            self.color = color
+            self.time_out = time_out
+            self.candidate_list = []
+            self.candidate_set = []
 
         # @jit()
         def go(self, chessboard):
-                self.candidate_list.clear()
-                # =================================================== #
-                f_rows, f_cols = np.where(chessboard == self.color)
-                for i in range(len(f_rows)):
-                    for drct in directions:
-                        row = f_rows[i] + drct[0]
-                        col = f_cols[i] + drct[1]
-                        if row < 0 or col < 0 or row >= self.chessboard_size or col >= self.chessboard_size or chessboard[row][col] != -self.color:
-                            continue
-                        while row >= 0 and col >= 0 and row < self.chessboard_size and col < self.chessboard_size and chessboard[row][col] == -self.color:    
-                            row += drct[0]
-                            col += drct[1]
-                        if row >= 0 and col >= 0 and row < self.chessboard_size and col < self.chessboard_size and chessboard[row][col] == COLOR_NONE:
-                            self.candidate_list.append((row, col))
+            self.candidate_list.clear()
+            # =================================================== #
+            f_rows, f_cols = np.where(chessboard == self.color)
+            for i in range(len(f_rows)):
+                for drct in directions:
+                    row = f_rows[i] + drct[0]
+                    col = f_cols[i] + drct[1]
+                    if row < 0 or col < 0 or row >= self.chessboard_size or col >= self.chessboard_size or chessboard[row][col] != -self.color:
+                        continue
+                    while row >= 0 and col >= 0 and row < self.chessboard_size and col < self.chessboard_size and chessboard[row][col] == -self.color:    
+                        row += drct[0]
+                        col += drct[1]
+                    if row >= 0 and col >= 0 and row < self.chessboard_size and col < self.chessboard_size and chessboard[row][col] == COLOR_NONE:
+                        self.candidate_list.append((row, col))
+            # =================================================== #
